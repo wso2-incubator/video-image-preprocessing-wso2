96d214105231de122c1d41c87743c4ae9dad49a3
Anusha Jayasundara <anushaj@wso2.com>
2016-12-22 23:27:51 +0530

    Update retrain.sh

--- Tensorflow_Plane_Retrain/retrain.sh
+++ Tensorflow_Plane_Retrain/retrain.sh
@@ -5,4 +5,4 @@
 bazel build -c opt --copt=-mavx tensorflow/examples/image_retraining:retrain
 #When the first command is completed retrainer can be run using this command.
 ##Before run this, set up the image folder path to Plane
-bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir ~/Plane
+bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir Plane
